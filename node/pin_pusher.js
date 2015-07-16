574564a22146cea92a0285b859e663cc9d74288d
root <root@dragonsden.edwinfinch.com>
2015-07-15 23:17:54 -0400

    Pushing pins works and show stack

--- node/pin_pusher.js
+++ node/pin_pusher.js
@@ -19,12 +19,16 @@ var showLocation = "NBC";
 var showTitle = "Chuck: S02E01";
 var edgyTopics = ["chuck"];
 
+function replaceAll(find, replace, str) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
 function constructIDFromShow(show){
     var id = "";
     id += show.attributes.channel;
     id += show.attributes.start.substring(3, 9);
     id += show.title;
-    return id;
+    return replaceAll(" ", "", id).toLowerCase();
 }
 
 function constructLengthFromShow(show){
@@ -48,56 +52,90 @@ function getStartFromShow(show){
     var startString = show.attributes.start;
     var newStartString = startString.substring(0, 4) + "/" + startString.substring(4, 6) + "/" + startString.substring(6, 8) + " " + startString.substring(8, 10) + ":" + startString.substring(10, 12);
     var start = new Date(newStartString);
-
     return start;
 }
 
 var database = mongo('tv', ['base']);
+var shows;
+var currentShow = 0, startTime = 0;
 
-database.base.aggregate( {$unwind : "$programme"},     {$match : { "programme.title" : "News" } },    {$group : { "_id" : "$_id" , "programme" : { $push: "$programme" } } }, function(something, docs){
-    var show = docs[0].programme[0];
+database.base.aggregate({$unwind : "$programme"},     {$match : {  } },    {$group : { "_id" : "$_id" , "programme" : { $push: "$programme" } } }, function(something, docs){
+    shows = docs[0].programme;
+    console.log("Amount of shows: " + shows.length);
+    var show = shows[0];
     console.log("show: " + JSON.stringify(show));
     console.log(constructIDFromShow(show));
-    console.log(constructLengthFromShow(show))
+    console.log(constructLengthFromShow(show));
+
+    pushNextShow();
 });
 
-/*
-// handler for GET /senduserpin/:userToken/:minutesToAdd?
-app.get('/senduserpin/', function (req, res) {
-  // create the pin object
-  var pin = new Timeline.Pin({
-    id: constructIDFromShow(currentShow),
-    time: getStartFromShow(currentShow),
-    duration: constructLengthFromShow(currentShow),
-    layout: {
-      type: "calendarPin",
-      tinyIcon: "system://images/TV_SHOW",
-      largeIcon: "system://images/TV_SHOW",
-      locationName: currentShow.attributes.channel,
-      title: currentShow.title
+function pushNextShow(){
+    var d = new Date();
+    startTime = d.getTime();
+    if(currentShow < 5){
+        pushShow(shows[currentShow]);
     }
-  });
+    currentShow++;
+}
 
-  // add actions to the pin
+// handler for GET /senduserpin/:userToken/:minutesToAdd?
+//app.get('/senduserpin/', function (req, res) {
+function pushShow(currentShow){
+  // create the pin object
+  var id = constructIDFromShow(currentShow);
+  var start = getStartFromShow(currentShow);
+  var duration = constructLengthFromShow(currentShow);
+  var type = "calendarPin";
+  var icon = "system://images/TV_SHOW";
+  var title = currentShow.title;
+  var channel = currentShow.attributes.channel;
 
-    pin.addAction(new Timeline.Pin.Action({
-      type: Timeline.Pin.ActionType.OPEN_WATCH_APP,
-      title: "Stop this show",
-      launchCode: 400
-    }));
+  var jsonPin = {
+     id: id,
+     time: start,
+     duration: duration,
+     layout: {
+        type: type,
+        tinyIcon: icon,
+        largeIcon: icon,
+        locationName: channel,
+        title: title,
+      }
+  };
+  var jsonString = JSON.stringify(jsonPin);
+  var jsonPinFinal = JSON.parse(jsonString);
+  console.log("Pin: " + jsonString + " and object " + jsonPinFinal);
+
+  var pin = new Timeline.Pin(jsonPin);
 
   // send the pin
   timeline.sendSharedPin(edgyTopics, pin, function (err, body, resp) {
     if (err) {
+        pushNextShow();
+        console.log(body);
       return console.error(err);
     }
-
-    res.send('Status code: ' + resp.statusCode);
+    var d = new Date();
+    var endTime = d.getTime();
+    console.log("Completed in " + (endTime-startTime) + " milliseconds.");
+    //res.send('Status code: ' + resp.statusCode);
+    pushNextShow();
   });
-});
-
+}
+//});
+/*
+ * Depreciated shit I don't want to lose
+ * rip in spret wins
 // start the webserver
 var server = app.listen(app.get('port'), function () {
-  console.log('timeline-user example app listening on port %s', app.get('port'));
+  console.log('PinPusher listening on port %s', app.get('port'));
 });
+
+// add actions to the pin
+  pin.addAction(new Timeline.Pin.Action({
+    type: Timeline.Pin.ActionType.OPEN_WATCH_APP,
+    title: "Stop this show",
+    launchCode: 400
+  }));
 */
